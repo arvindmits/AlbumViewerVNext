2b89adb919fc9d3999f39de06ab74d9d78e18485
Tim Jarzombek <tim@jarz.net>
2019-12-30 20:47:52 -0500

    Add App Insights to AspNetCore

--- src/AlbumViewerNetCore/Startup.cs
+++ src/AlbumViewerNetCore/Startup.cs
@@ -148,7 +148,8 @@ namespace AlbumViewerNetCore
                     if (HostingEnvironment.IsDevelopment())
                         opt.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
                 });
-           
+            services.AddApplicationInsightsTelemetry();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
