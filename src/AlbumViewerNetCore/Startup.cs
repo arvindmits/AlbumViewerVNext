ea4fb78558c0ee1c0f76b04063ba765e76e1ff4f
Tim Jarzombek <tim@jarz.net>
2020-01-07 16:41:50 -0500

    Switch to SQL Server localdb

--- src/AlbumViewerNetCore/Startup.cs
+++ src/AlbumViewerNetCore/Startup.cs
@@ -18,6 +18,7 @@ using Serilog;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.Extensions.Hosting;
+using Microsoft.ApplicationInsights.Extensibility;
 
 namespace AlbumViewerNetCore
 {
